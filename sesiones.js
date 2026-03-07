836c92d2fa54d472e58d127583411bf5754ccdb0
sergioesv <95048041+sergioesv@users.noreply.github.com>
2026-03-06 20:19:47 -0500

    Update sesiones.js

--- sesiones.js
+++ sesiones.js
@@ -1,6 +1,18 @@
 var sesiones = new Map();
 
+var TIMEOUT_MS = 30 * 60 * 1000; // 30 minutos
+
 function obtenerSesion(telefono) {
+  // Check if session exists and is expired
+  if (sesiones.has(telefono)) {
+    var sesion = sesiones.get(telefono);
+    var ahora = Date.now();
+    if (ahora - sesion.ultimaActividad > TIMEOUT_MS) {
+      console.log('Sesion expirada para ' + telefono + ' (' + Math.round((ahora - sesion.ultimaActividad) / 60000) + ' min inactivo)');
+      sesiones.delete(telefono);
+    }
+  }
+
   if (!sesiones.has(telefono)) {
     sesiones.set(telefono, {
       estado: 'INICIO',
@@ -14,9 +26,14 @@ function obtenerSesion(telefono) {
       fotosNovedadPendientes: [],
       observacion: null,
       grupoActual: 0,
-      fotoVerificacionDescripcion: null
+      fotoVerificacionDescripcion: null,
+      ultimaActividad: Date.now()
     });
+  } else {
+    // Update last activity
+    sesiones.get(telefono).ultimaActividad = Date.now();
   }
+
   return sesiones.get(telefono);
 }
 
@@ -36,5 +53,20 @@ function copiarSesion(sesion) {
     observacion: sesion.observacion
   };
 }
+
+// Cleanup expired sessions every 10 minutes
+setInterval(function() {
+  var ahora = Date.now();
+  var eliminadas = 0;
+  sesiones.forEach(function(sesion, telefono) {
+    if (ahora - sesion.ultimaActividad > TIMEOUT_MS) {
+      sesiones.delete(telefono);
+      eliminadas++;
+    }
+  });
+  if (eliminadas > 0) {
+    console.log('Limpieza: ' + eliminadas + ' sesion(es) expirada(s) eliminada(s)');
+  }
+}, 10 * 60 * 1000);
 
 module.exports = { obtenerSesion, eliminarSesion, copiarSesion };
