46b114e060aca491902e06907401054476b7a333
sergioesv <95048041+sergioesv@users.noreply.github.com>
2026-03-08 18:40:32 -0500

    Update sesiones.js

--- sesiones.js
+++ sesiones.js
@@ -1,14 +1,14 @@
 var sesiones = new Map();
+var procesando = new Set(); // bloqueo por telefono
 
 var TIMEOUT_MS = 30 * 60 * 1000; // 30 minutos
 
 function obtenerSesion(telefono) {
-  // Check if session exists and is expired
   if (sesiones.has(telefono)) {
     var sesion = sesiones.get(telefono);
     var ahora = Date.now();
     if (ahora - sesion.ultimaActividad > TIMEOUT_MS) {
-      console.log('Sesion expirada para ' + telefono + ' (' + Math.round((ahora - sesion.ultimaActividad) / 60000) + ' min inactivo)');
+      console.log('Sesion expirada para ' + telefono);
       sesiones.delete(telefono);
     }
   }
@@ -30,7 +30,6 @@ function obtenerSesion(telefono) {
       ultimaActividad: Date.now()
     });
   } else {
-    // Update last activity
     sesiones.get(telefono).ultimaActividad = Date.now();
   }
 
@@ -39,6 +38,7 @@ function obtenerSesion(telefono) {
 
 function eliminarSesion(telefono) {
   sesiones.delete(telefono);
+  procesando.delete(telefono);
 }
 
 function copiarSesion(sesion) {
@@ -54,19 +54,31 @@ function copiarSesion(sesion) {
   };
 }
 
-// Cleanup expired sessions every 10 minutes
+// Bloqueo de concurrencia por telefono
+function bloquear(telefono) {
+  if (procesando.has(telefono)) return false; // ya ocupado
+  procesando.add(telefono);
+  return true;
+}
+
+function desbloquear(telefono) {
+  procesando.delete(telefono);
+}
+
+// Cleanup cada 10 minutos
 setInterval(function() {
   var ahora = Date.now();
   var eliminadas = 0;
   sesiones.forEach(function(sesion, telefono) {
     if (ahora - sesion.ultimaActividad > TIMEOUT_MS) {
       sesiones.delete(telefono);
+      procesando.delete(telefono);
       eliminadas++;
     }
   });
   if (eliminadas > 0) {
-    console.log('Limpieza: ' + eliminadas + ' sesion(es) expirada(s) eliminada(s)');
+    console.log('Limpieza: ' + eliminadas + ' sesion(es) expirada(s)');
   }
 }, 10 * 60 * 1000);
 
-module.exports = { obtenerSesion, eliminarSesion, copiarSesion };
+module.exports = { obtenerSesion, eliminarSesion, copiarSesion, bloquear, desbloquear };
